b35e20021d0f95d4ab43be67dc83d9cdf7b41a48
Amos-ongeri <amosongeri041@gmail.com>
2025-11-28 16:51:23 +0300

    small change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/forecast', forecast)
 app.use('/api/geolocation', geoRoute)
 app.use("/api/tiles", tileRoutes);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
     
